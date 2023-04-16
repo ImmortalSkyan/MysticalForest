9c734af07b3d90ec8de96406cabeb864b407ef29
SkyStich <65466062+SkyStich@users.noreply.github.com>
2023-04-16 14:55:27 +0300

    Create range weapon class

---
+++ Source/MysticalForest/Public/Actors/Weapons/RangeWeaponActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Weapons/BaseWeaponActor.h"
+#include "RangeWeaponActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class MYSTICALFOREST_API ARangeWeaponActor : public ABaseWeaponActor
+{
+	GENERATED_BODY()
+	
+};
